b6e81918cd64a6c2cbb9858d6f431bd2c1aa5b65
acascoc098 <acascoc098@g.educaand.es>
2023-12-18 18:07:18 +0100

    Creamos el viewholder.

--- app/src/main/java/com/example/aplicacionv1_2/adapter/ViewBar.kt
+++ app/src/main/java/com/example/aplicacionv1_2/adapter/ViewBar.kt
@@ -2,11 +2,38 @@ package com.example.aplicacionv1_2.adapter
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.aplicacionv1_2.databinding.ItemBarBinding
+import com.example.aplicacionv1_2.models.Bar
 
 class ViewBar (view: View,
                var deleteOnClick: (Int) -> Unit,
                var updateOnClick: (Int) -> Unit):
     RecyclerView.ViewHolder (view){
 
-        var binding
+        var binding: ItemBarBinding
+        init {
+            binding = ItemBarBinding.bind(view)
+        }
+    fun renderize(bar: Bar, position: Int){
+        binding.txtviewName.setText(bar.name)
+        binding.txtviewCity.setText(bar.city)
+        binding.txtviewProvince.setText(bar.province)
+        binding.txtviewPhone.setText(bar.phone)
+        Glide
+            .with( itemView.context)
+            .load(bar. image)
+            .centerCrop()
+            .into( binding.ivBar)
+        setOnClickListener(position)
+    }
+
+    private fun setOnClickListener(position: Int) {
+        binding.btnEdit.setOnClickListener {
+            updateOnClick(position )
+        }
+        binding.btnDelete.setOnClickListener {
+            deleteOnClick(position)
+        }
+    }
 }
