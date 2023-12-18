a21bf01abc286e7800ae247d63ba91a4d427639c
acascoc098 <acascoc098@g.educaand.es>
2023-12-18 18:39:03 +0100

    Objeto para los argumentos

---
+++ app/src/main/java/com/example/aplicacionv1_2/models/ArgumentosBar.kt
@@ -0,0 +1,9 @@
+package com.example.aplicacionv1_2.models
+
+object ArgumentosBar {
+    const val ARGUMENT_NAME = "name"
+    const val ARGUMENT_CITY = "city"
+    const val ARGUMENT_PROVINCE ="province"
+    const val ARGUMENT_PHONE ="phone"
+    const val ARGUMENT_IMAGE = "image"
+}
