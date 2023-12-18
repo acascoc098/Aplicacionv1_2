eaac320009f4b03e2ab8e6a6f8bfd3d9cb6605cd
acascoc098 <acascoc098@g.educaand.es>
2023-12-18 18:19:12 +0100

    Creamos el repositorio

---
+++ app/src/main/java/com/example/aplicacionv1_2/object_models/Repository.kt
@@ -0,0 +1,23 @@
+package com.example.aplicacionv1_2.object_models
+
+import com.example.aplicacionv1_2.models.Bar
+
+object Repository {
+    val listBares : List<Bar> = listOf(
+        Bar(
+            "El Paso", "Cazorla", "Jaén", "123456789", ""
+        ),
+        Bar(
+            "La Toba", "Cazorla", "Jaén", "123456789", ""
+        ),
+        Bar(
+            "Casa Tino", "Cazorla", "Jaén", "123456789", ""
+        ),
+        Bar(
+            "La Cantina", "Cazorla", "Jaén", "123456789", ""
+        ),
+        Bar(
+            "La Yedra", "Cazorla", "Jaén", "123456789", ""
+        )
+    )
+}
